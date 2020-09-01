bd2341cb783d6894daee4f0e30f1cc69d8f886d4
Leszek Kicior <leszek.kicior@fluentbe.com>
2020-09-01 21:42:09 +0200

    Tests improved to work with new parsing-included methods

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,6 @@
 import api from "../index.js";
 import chai from 'chai'
-import longVodka from "./longVodka.js";
+import { longVodka, longVodkaJson } from "./longVodka.js";
 
 const expect = chai.expect
 
@@ -8,20 +8,26 @@ describe('API requests', function () {
     describe('searchCocktail', function () {
         it('should return drink list', function () {
             return api.searchCocktail('margarita').then(data => {
-                expect(data.drinks).to.be.an('Array')
-                expect(data.drinks.length).to.be.above(0)
+                expect(data).to.be.an('Array')
+                expect(data.length).to.be.above(0)
             })
         });
         it('should return null when no drinks found', function () {
             return api.searchCocktail('test123123').then(data => {
-                expect(data.drinks).to.be.an('null')
+                console.log(data)
+                expect(data).to.be.an('null')
             })
         });
     });
     describe('lookupCocktail', function () {
         it('should return Long Vodka recipe', function () {
             return api.lookupCocktail(13196).then(data => {
-                expect(data.drinks[0]).to.deep.equal(longVodka.jsonResponse)
+                expect(data).to.deep.equal(longVodka)
+            })
+        });
+        it('should return false when no cocktail found', function () {
+            return api.lookupCocktail(13196).then(data => {
+                expect(data).to.deep.equal(longVodka)
             })
         });
     });
@@ -29,10 +35,10 @@ describe('API requests', function () {
 
 describe('Drink parser', function () {
     it('should parse drink name', function () {
-        expect(api.parseDrink(longVodka.jsonResponse).name).to.equal(longVodka.name)
+        expect(api.parseDrink(longVodkaJson).name).to.equal(longVodka.name)
     })
     it('should parse drink ingredients', function () {
-        expect(api.parseDrink(longVodka.jsonResponse).ingredients).to.deep.equal(longVodka.ingredients)
+        expect(api.parseDrink(longVodkaJson).ingredients).to.deep.equal(longVodka.ingredients)
     })
     it('should return false on empty JSON', function () {
         expect(api.parseDrink('')).to.be.false
